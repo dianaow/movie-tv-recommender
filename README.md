5b4ab9e8402bf161893392d2acae5a52c480c7b4
dianaow <diana.ow@outlook.com>
2021-09-04 21:38:29 +0800

    first commit

--- README.md
+++ README.md
@@ -14,21 +14,16 @@
 `source <path/to/new/virtualenv>/bin/activate
 pip install -r <path/to/requirement.txt>`
 
-3) To create and store recommendation system algorithm output files (This only needs to be run once)
-
-`python script.py`
-
-
 #### Launch App
 
-4) Install all required packages and dependencies
+3) Install all required packages and dependencies
 
 `npm install`
 
-5) Start up the web server
+4) Start up the web server
 
 `node server.js`
 
-6) Open http://localhost:8080 to view the webpage in the browser. You may begin querying for similar Netflix shows through the search bar.
+5) Open http://localhost:8080 to view the webpage in the browser. You may begin querying for similar Netflix shows through the search bar.
 
 ![alt text](https://github.com/dianaow/netflix-recommender/raw/master/example.gif "Demo")
--- server.js
+++ server.js
@@ -1,38 +1,23 @@
-const neo4j = require('neo4j-driver')
+// load all env variables from .env file into process.env object.
+require(‘dotenv’).config()
+
 const express = require("express")
 const cors = require('cors')
-const fs = require("fs")
 const path = require("path")
-const unirest = require("unirest");
-const {spawn} = require('child_process');
 
 const app = express()
 app.use(cors())
 const PORT = process.env.PORT || 8080
-const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT
 
-const protocol="bolt"
-const host = "localhost"
-const port="7687"
-const user="neo4j"
-const password="testing"
-const driver = neo4j.driver(protocol + "://" + host + ":" + port, neo4j.auth.basic(user, password))
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'dianaow',
+  host: 'localhost',
+  database: 'test',
+  //password: 'password',
+  port: 5432,
+})
 
-async function run_request(cypher) {
-  let results
-  let session = driver.session()
-  await session.run(cypher)
-    .then(r => {
-      results = r.records.map(d => d.toObject())
-      session.close()
-    })
-    .catch(e => {
-      console.log(e)
-      session.close()
-    })
-  return results
-
-}
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*")
@@ -44,60 +29,28 @@ app.use(function (req, res, next) {
   next()
 })
 
-app.get("/actors/:country/:number", async function (req, res, next) {
-
-  let country = req.params.country
-  country = country.replace("_", " ")
-
-  const request=`
-    MATCH (p:Person)-[rel:ACTED_IN]->(m:Movie)
-    WHERE m.country = '${country}'
-    WITH p,collect(m.title) as movies,count(*) as total
-    RETURN  p.name, movies,total
-    ORDER BY total DESC
-    LIMIT ${req.params.number}
-  `
-  let results = await run_request(request)
-
-  return res.send(results)
-
-})
-
-app.get("/path/:from/:to", async function (req, res, next) {
-
-  let from = req.params.from
-  from = from.replace("_", " ")
-  let to = req.params.to
-  to = to.replace("_", " ")
-
-  const request=`
-    MATCH (cs:Person { name: "${from}" }),(ms:Person { name: "${to}" }), p = shortestPath((cs)-[:ACTED_IN|:DIRECTED*]-(ms))
-    WHERE length(p)> 1 
-    RETURN p
-  `
-  let results = await run_request(request)
-
-  return res.send(results)
-
-})
-
 app.get('/similarity', (req, res) => {
-   console.log('Query entered')
-   let largeDataSet = []
-   let spawn = require('child_process').spawn;
-   let python = spawn('python', ['./query.py', req.query.title.replace('%20', ' '), req.query.w1, req.query.w2, req.query.w3, req.query.w4]);
 
-   // collect data from script
-   python.stdout.on('data', function (data) {
-    console.log('Pipe data from python script ...');
-    largeDataSet.push(data);
-   });
-   // in close event we are sure that stream from child process is closed
-   python.on('close', (code) => {
-    console.log(`child process close all stdio with code ${code}`);
-    // send data to browser
-    console.log(largeDataSet.join(""))
-    res.send(largeDataSet.join(""))
+  const title = req.query.title
+
+  pool.query('SELECT * FROM results WHERE search_title = $1', [title], (error, results) => {
+    if (error) {
+      throw error
+    }
+    res.send(results.rows)
+  })
+
+})
+
+app.get('/title', (req, res) => {
+
+  const title = req.query.title
+
+  pool.query('SELECT * FROM titles WHERE title = $1', [title], (error, results) => {
+    if (error) {
+      throw error
+    }
+    res.send(results.rows)
   })
 
 })
